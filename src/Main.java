64e27e75ee965687295354cfce6ad724d61c7196
mouta-a <moutasem.a.h@live.se>
2022-11-08 16:27:43 +0100

    Change in branch

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world! Change main 8 from a merge conflict");
+        System.out.println("This is written in a new branch");
 
 
 
