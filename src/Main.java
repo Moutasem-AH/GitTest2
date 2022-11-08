3fa03c9ab0e95a06ddbad4dcc6f7d369acb86271
mouta-a <moutasem.a.h@live.se>
2022-11-08 16:17:34 +0100

    Change 6

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! Change main 5 from original");
+        System.out.println("Hello world! Change main 6 from copy");
 
 
 
